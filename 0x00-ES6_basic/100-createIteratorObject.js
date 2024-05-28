7f04073e80029a27f23cc548b37c646cbc637a56
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-05-28 18:52:05 +0100

    iterator object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,8 @@
 export default function createIteratorObject(report) {
-    const myarr = [];
-    for (const employee of Object.values(report.allEmployees)){
-        myarr.push(...employee);
-    }
+  const myarr = [];
+  for (const employee of Object.values(report.allEmployees)) {
+    myarr.push(...employee);
+  }
 
-    return myarr;
-
+  return myarr;
 }
