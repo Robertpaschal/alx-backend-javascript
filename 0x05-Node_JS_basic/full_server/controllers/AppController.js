79fa00bd592d0b261d53736d9887632397690403
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-23 17:52:45 +0100

    full app server

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -3,11 +3,10 @@
  * @author Odinaka Nnamani
  */
 class AppController {
-    static getHomepage(request, response) {
-      response.status(200).send('Hello Holberton School!');
-    }
+  static getHomepage (request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
-  
-  export default AppController;
-  module.exports = AppController;
-  
+}
+
+export default AppController;
+module.exports = AppController;
