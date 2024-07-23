9434cdaac22d1fb058d5b23cf812658b3919266a
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-23 17:13:34 +0100

    reading a file synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+/**
+ * Reads a database synchronously
+ */
+const fs = require('fs');
+
+function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const header = lines.shift();
+
+    if (!header) {
+      throw new Error('Cannot load the database');
+    }
+    const studentsByField = {};
+    lines.forEach((line) => {
+      const [firstName, lastname, age, field] = line.split(',');
+
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
+      studentsByField[field].push(firstName);
+    });
+
+    const totalStudents = lines.length;
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, students] of Object.entries(studentsByField)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
