cf1ff79a80c22ae7c9ceb4e231f153f54cf7c591
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-23 12:28:33 +0100

    process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+/**
+ * Dsiplays and executes a program thorugh command line
+ */
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (input) => {
+    const name = input.trim();
+    console.log(`Your name is: ${name}`);
+
+    console.log('This important software is now closing');
+    process.exit();
+});
+
+process.on('SIGINT', () => {
+    console.log('/nThis important software is now closing');
+    process.exit();
+});
