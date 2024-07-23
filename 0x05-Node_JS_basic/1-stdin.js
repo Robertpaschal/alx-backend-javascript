be83b5b9730d3c12ea3e8a19395e6ac72688d3f9
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-23 12:38:11 +0100

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
 
   process.stdout.write('This important software is now closing\n');
   process.exit();
