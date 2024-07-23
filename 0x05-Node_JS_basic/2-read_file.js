f9b1d8297d6cbcdce551b95173919094647a43c1
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-23 17:22:06 +0100

    reading a file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
 const fs = require('fs');
 /**
  * Reads a database synchronously
  */
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter(line => line.trim() !== '');
