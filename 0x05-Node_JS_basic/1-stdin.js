f00a5b625752d14583009e599f46bcdee27e6c5a
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-23 12:42:33 +0100

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,7 +9,9 @@ process.stdin.on('data', (input) => {
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
-
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+})
