8d03b6b88439c0d5dd8db209234f75f06cb68ba7
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-23 13:27:32 +0100

    process stdin

---
+++ 0x05-Node_JS_basic/test/test_1.js
@@ -0,0 +1,19 @@
+const { spawn } = require('child_process');
+
+const child = spawn('./1-stdin.js');
+
+child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
+setTimeout(() => {
+    child.stdin.write('Alice\n');
+}, 1000);
